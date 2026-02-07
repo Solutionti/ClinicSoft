7676d4274f614907ce78925cc95bcea53c0c364e
Desarrollador Jerson Galvez <desarrollo.gocho@gmail.com>
2026-02-07 10:14:12 -0500

    separando el codigo

--- application/views/login.php
+++ application/views/login.php
@@ -12,7 +12,6 @@
     <link href="<?php echo base_url(); ?>public/fontawesome/css/brands.css" rel="stylesheet">
   	<link href="<?php echo base_url(); ?>public/fontawesome/css/solid.css" rel="stylesheet">
   <style>
-    
     .color-rosa {
       background-color: #CF1B77 !important;
     }
@@ -25,28 +24,23 @@
     .rosa-text {
       color: #CF1B77 !important;
     }
-    
+    body {
+      background-color: #fbf8ff;
+      background-image: url("data:image/svg+xml,%3Csvg width='100' height='100' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11 18c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm48 25c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm-43-7c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm63 31c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM34 90c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm56-76c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM12 86c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm28-65c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm23-11c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-6 60c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm29 22c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zM32 63c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm57-13c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-9-21c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM60 91c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM35 41c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM12 60c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2z' fill='%231d0643' fill-opacity='0.03' fill-rule='evenodd'/%3E%3C/svg%3E");
+    }
   </style>
 </head>
 <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * -->
 <!-- * * * * * * * * * * * JERSON GALVEZ ENSUNCHO * * * * * * * * * * * -->
 <!-- ******* * * * * * * * PROYECTO CLINICSOFT * * * * * * * * * * * * * -->
 <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-<div  [hidden]="spinner" class="overlay z-index">
-  <div class="dot-wave">
-    <div class="dot-wave__dot"></div>
-    <div class="dot-wave__dot"></div>
-    <div class="dot-wave__dot"></div>
-    <div class="dot-wave__dot"></div>
-  </div>
-</div>
 <main class="main-content  mt-0">
   <div class="container-fluid">
   <div
     class="position-fixed"
     style="z-index:50;"
   >
-    <a class="btn btn-primary btn-sm text-white" target="blank" href="https://wa.me/+51965291084">
+    <a class="btn btn-dark btn-sm text-white " target="blank" href="https://wa.me/+51965291084">
       <i class="fab fa-whatsapp"></i> 
       Soporte y ventas
     </a>
@@ -84,7 +78,7 @@
       <h5 class="font-weight-bolder  mt-4">Iniciar sesión</h5>
     </div>
     <div class="card-body">
-    <div class="messageError"></div>
+      <div class="messageError"></div>
     <form role="form" method="post" validate id="FormLOG">
       <div class="mb-3">
         <input type="text" required class="form-control" id="correo" value=""  placeholder="Usuario">
